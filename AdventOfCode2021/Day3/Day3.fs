0779e1785cc8dc601a2bd44d529108cf4b36bf5d
zh6335901 <zh6335901@hotmail.com>
2022-06-15 23:23:19 +0800

    Use Seq.zip to improve day3 puzzle5

--- AdventOfCode2021/Day3/Day3.fs
+++ AdventOfCode2021/Day3/Day3.fs
@@ -7,26 +7,24 @@ let input = File.ReadAllLines("Day3/input.txt")
     
 module Puzzle5 =
     let initCounter: int list = [ for i in 1..input[0].Length -> 0 ]
-    let half = input.Length / 2
+    let total = input.Length
 
-    let rec countBinary counter binary =
-        match counter, binary with
-        | c :: ctail, b :: btail -> (c + b) :: (countBinary ctail btail)
-        | _, _ -> []
+    let aggr counter binary =
+        binary
+        |> Seq.zip counter
+        |> Seq.map (fun (i, ch) -> i + if ch = '1' then 1 else 0)
 
-    let charToInt c = int c - int '0'
+    let counter = input |> Seq.fold aggr initCounter
 
     let gammaBits = 
-        List.ofArray input
-        |> List.map (fun l -> l.ToCharArray() |> (Array.map charToInt) |> List.ofArray)
-        |> List.fold countBinary initCounter
-        |> List.map (fun c -> if c >= half then '1' else '0')
+        counter
+        |> Seq.map (fun c -> if c >= total - c  then '1' else '0')
 
     let epsilonBits = 
-        gammaBits |> List.map (fun b -> if b = '1' then '0' else '1')
+        gammaBits |> Seq.map (fun b -> if b = '1' then '0' else '1')
 
-    let gs = String(gammaBits |> Array.ofList)
-    let es = String(epsilonBits |> Array.ofList)
+    let gs = String(gammaBits |> Seq.toArray)
+    let es = String(epsilonBits |> Seq.toArray)
 
     let gammaNum = Convert.ToInt32(gs, 2)
     let epsilonNum = Convert.ToInt32(es, 2)
